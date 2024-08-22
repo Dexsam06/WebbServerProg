159b5334c4f18b18bcadf439f10a5d921f9d3759
Samuel <samuel@dexe.se>
2024-08-22 13:36:11 +0200

    init

---
+++ www/public/Moment-0/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8" />
+    <title>Quiz med Formulär och PHP</title>
+</head>
+<body>
+<h1>Quiz</h1>
+<form action="result.php" method="post">
+    <label>I vilket språk programmerar vi i på kursen?<br>    
+        <input type="radio" name="qOne" value="php">PHP<br>
+        <input type="radio" name="qOne" value="asp">ASP<br>
+        <input type="radio" name="qOne" value="jsp">JSP<br><br>
+    </label>
+    <label>Vilket språk är ett klientskriptspråk<br>  
+        <input type="radio" name="qTwo" value="jsp">JSP<br>
+        <input type="radio" name="qTwo" value="js">JavaScript<br>
+        <input type="radio" name="qTwo" value="basic">Basic<br><br>
+    </label>
+    <input type="submit" value="Skicka" />
+        
+    <a href="result.php?id=21&name=kalle&age=13">Nästa</a>
+</form>
+</body>
+</html>
---
+++ www/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StartSida</title>
+</head>
+<body>
+    <a href="Moment-0/index.php">Moment-0</a>
+</body>
+</html>
